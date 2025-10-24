e7a708f063490f8ed9f16b3b050fe210cc20245e
Urgot <madaevgerehan@gmail.com>
2025-10-24 14:33:02 +0300

    add try timeout call

--- src/handlers/grades/utils.py
+++ src/handlers/grades/utils.py
@@ -2,10 +2,13 @@
 import asyncio
 import time
 
+from aiohttp import ClientTimeout
+
 async def post_lesson(session, payload, start_time_lesson):
     try:
+        timeout = ClientTimeout(total=3)
         async with session.post(
-            "https://ecampus.ncfu.ru/studies/GetLessons", data=payload
+            "https://ecampus.ncfu.ru/studies/GetLessons", data=payload, timeout=timeout
         ) as grades_resp:
             end_time_lesson = time.perf_counter()
             elapsed_lesson = end_time_lesson - start_time_lesson
